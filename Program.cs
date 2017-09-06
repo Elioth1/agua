1325cedf2906b05b3d0718a502ff8c982479c75b
Elioth1 <31458423+Elioth1@users.noreply.github.com>
2017-09-05 19:13:57 -0500

    Add files via upload
    Hecho por Alejandra Lizette Gómez Tamayo y Elioth Marrujo de la Vega
    Ya lo tenía, pero ya ve que no podía poner el visual y no sabía si había errores :v

--- Program.cs
+++ Program.cs
@@ -1,32 +1,33 @@
 ﻿using System;
-
-namespace agua
+class program
 {
-    class Program
+ static void Main()
     {
-        static void Main(string[] args)
+      
+        Console.WriteLine("el programon");
+        int min, fecha, minu = 0;
+        for (int i = 1; i <= 1; i++)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("¿Cuantos minutos tardas en bañarte?: ");
+            min = Convert.ToInt32(Console.ReadLine());
+            minu = min * 12;
+            Console.WriteLine("¿El agua que gastas al dia equivale a esta cantidad de botellas de agua?: " + minu);
+            Console.ReadKey();
+
+            Console.WriteLine("¿Cual es tu fecha de cumpleaños?: (AÑO,MES,DIA sin espacio) : ");
+            fecha = Convert.ToInt32(Console.ReadLine());
+           
+            DateTime hoy = DateTime.Today;
+            DateTime cumple =  new DateTime(2018,01,19);
+            TimeSpan diaEntre = cumple - hoy;
+            System.Console.WriteLine(diaEntre.TotalDays);
+            double  promedio;
+            promedio = diaEntre.TotalDays * minu;
+            Console.WriteLine("El resultado de botellas que gastarias asta tu cumpleaños es de:" + promedio);
+            Console.ReadKey();
+            Console.WriteLine("Hecho por Alejandra Lizette Gómez Tamayo y Elioth Marrujo de la Vega");
 
         }
-
-
-
-        ///<summary>
-        ///Calcula el número de botella dependiendo de los minutos 
-        ///cada minuto equivale a 12 botellas
-        ///</summary>
-        static int calcularBotella(int minutos)
-        {
-            return 0;
-        }
-
-        ///<summary>
-        ///Calcula el número de días restantes hasta la fecha dada
-        ///</summary>
-        static int calcularDiasRestantes(DateTime fechaCumple)
-        {
-            return 0;
-        }
+        
     }
 }
